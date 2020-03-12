bb1f4a0ada755aac62dfd648ab5eae05f50ead59
Shannon <scrowley016@gmail.com>
2020-03-12 15:48:06 -0400

    getting started complete

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const morgan = require("morgan")
 const express = require("express");
+const layout = require("./views/layout");
 
 const app = express();
 
 app.use(morgan("dev"));
-app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    res.send(layout("Hello World"));
   })
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
-const PORT = 1337;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
